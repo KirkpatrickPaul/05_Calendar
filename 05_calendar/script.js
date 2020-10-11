de610b6c053f0d1745e494bd8e5812456abd7f44
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-11 17:44:44 -0500

    Last commit broke HTML. I fixed HTML and started on save function.

--- 05_calendar/script.js
+++ 05_calendar/script.js
@@ -1,29 +1,37 @@
-var date = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").text(date);
-//the hour is converted to a number by multiplying by 1.
-var time = 1 * moment().format("HH");
-//this function fills colors based on the hour of the time variable.
-for (var i = 8; i <= 17; i++) {
-  var $cell = $("#hour-" + i);
-  var text = localStorage.getItem("hour" + i);
-  // the following function removes the null value for empty storage spaces
-  if (text === null) {
-    text = "";
+$(document).ready(function () {
+  var date = moment().format("dddd, MMM Do YYYY");
+  $("#currentDay").text(date);
+  //the hour is converted to a number by multiplying by 1.
+  var time = 1 * moment().format("HH");
+  //this function fills colors based on the hour of the time variable.
+  for (var i = 8; i <= 17; i++) {
+    var $cell = $(".hr-" + i);
+    var text = localStorage.getItem("hour" + i);
+    // the following function removes the null value for empty storage spaces
+    if (text === null) {
+      text = "";
+    }
+    $cell.html(
+      '<textarea class="form-control-plaintext hr-' +
+        i +
+        '" type="text" rows=3>' +
+        text +
+        "</textarea>"
+    );
+    //the following function fills colors based on the hour of the time variable.
+    if (i > time) {
+      $cell.attr("class", "col-8 hour future");
+    } else if (i === time) {
+      $cell.attr("class", "col-8 hour present");
+    } else {
+      $cell.attr("class", "col-8 hour past");
+    }
   }
-  $cell.html(
-    '<textarea class="form-control-plaintext hour-' +
-      i +
-      '" type="text" rows=3>' +
-      text +
-      "</textarea>"
-  );
-  //the following function fills colors based on the hour of the time variable.
-  if (i > time) {
-    $cell.attr("class", "col-8 hour future");
-  } else if (i === time) {
-    $cell.attr("class", "col-8 hour present");
-  } else {
-    $cell.attr("class", "col-8 hour past");
-  }
-}
-$
+  $(".saveBtn").on("click", function (target) {
+    for (var i=8; i<=17; i++) {
+      if (target.matches("hour" + i) {
+        var 
+      }
+    }
+  });
+});
